76eb9437222ad20fe6d001814e83f49b9dcbe8cc
Yutika2309 <yutikarege2309@gmail.com>
2024-09-01 17:12:42 +0530

    serializer method field

--- watchlist_app/serializers.py
+++ watchlist_app/serializers.py
@@ -4,9 +4,29 @@ from watchlist_app.models import *
 
 class MovieSerializer(serializers.ModelSerializer):
 
+    len_name = serializers.SerializerMethodField()
+
     class Meta:
         model = Movies
-        fields = "__all__"
+        # fields = '__all__'
+        # fields = ['id', 'name', 'description']
+        exclude = ['created_at', 'updated_at']
+
+    def get_len_name(self, instance):
+        length = len(instance.name)
+        return length
+
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        else:
+            return value
+    
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Movie Name and Description cannot be the same")
+        else:
+            return data 
 
 # def name_length(value):
 #     if len(value) < 2:
